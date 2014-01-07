d927409d4629aab78396bb87b5aef37a4c505da1
Dominic Barnes <dbarnes@treetopllc.com>
2014-01-07 00:19:45 -0600

    removing case for Function, as it's covered by valueOf below

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ var toString = Object.prototype.toString;
 
 module.exports = function(val){
   switch (toString.call(val)) {
-    case '[object Function]': return 'function';
     case '[object Date]': return 'date';
     case '[object RegExp]': return 'regexp';
     case '[object Arguments]': return 'arguments';
@@ -24,6 +23,6 @@ module.exports = function(val){
   if (val === null) return 'null';
   if (val === undefined) return 'undefined';
   if (val && val.nodeType === 1) return 'element';
-  
+
   return typeof val.valueOf();
 };
